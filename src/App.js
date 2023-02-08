d69133c9029fec31dfb73db34a3d0f0d989e7767
Steven Bryant <stevelad_83@yahoo.com>
2023-02-08 13:29:34 -0500

    hooked up Posts component and Switch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Auth from './components/Auth.js';
+import Posts from './components/Posts.js';
 
 function App() {
   return (
     <div className="App">
       <h1>Bulletin Board</h1>
+      <Switch>
+        <Route path="/posts" component={Posts} />
+        <Route path="/auth/:type" component={Auth} />
+      </Switch>
+
       <Auth />
     </div>
   );
