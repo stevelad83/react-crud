57fe716599fa31e1e93354e97de3780af5033094
Steven Bryant <stevelad_83@yahoo.com>
2023-02-07 19:20:06 -0500

    built auth component userprovider and browser router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Auth from './components/Auth.js';
 
 function App() {
   return (
     <div className="App">
       <h1>Bulletin Board</h1>
+      <Auth />
     </div>
   );
 }
---
+++ src/components/Auth.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { Redirect, useParams } from 'react-router-dom';
+import { useUser } from '../context/UserContext.js';
+import { authUser } from '../services/auth.js';
+
+export default function Auth() {
+  //   const { type } = useParams();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { user, setUser } = useUser();
+
+  const clickHandler = async () => {
+    const userResp = await authUser(email, password);
+    setUser(userResp);
+    setEmail('');
+    setPassword('');
+  };
+
+  if (user) {
+    console.log('user', user);
+    return <Redirect to="/posts" />;
+  }
+
+  return (
+    <div>
+      <div className="form-controls">
+        <label>Email:</label>
+        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <div className="form-controls">
+        <label>Password:</label>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <button onClick={clickHandler}>Submit</button>
+    </div>
+  );
+}
