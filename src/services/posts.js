1b6b394eb0f9595e0b7b1014f1bc86621d4aa3f4
Steven Bryant <stevelad_83@yahoo.com>
2023-02-10 12:56:07 -0500

    created update post service

--- src/services/posts.js
+++ src/services/posts.js
@@ -8,6 +8,10 @@ export async function getPosts() {
 
 export async function createPost(title, description) {
   const response = await client.from('bulletins').insert({ title, description });
-  console.log('response', response);
+  return checkError(response);
+}
+
+export async function updatePost(id, title, description) {
+  const response = await client.from('bulletins').update({ title, description }).match({ id });
   return checkError(response);
 }
